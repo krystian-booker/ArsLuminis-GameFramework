c507b7821518577d256de12a9813593112e69e09
Krystian Booker <krystian.booker@gmail.com>
2021-08-12 07:39:48 -0400

    Added ChangeVirtualCamera node

--- Assets/Scripts/EventSystem/VisualEditor/Nodes/Camera/ChangeVirtualCameraNode.cs
+++ Assets/Scripts/EventSystem/VisualEditor/Nodes/Camera/ChangeVirtualCameraNode.cs
@@ -1,8 +1,5 @@
 using Cinemachine;
 using EventSystem.Models;
-using Sirenix.OdinInspector;
-using Sirenix.Serialization;
-using Tools;
 using UnityEngine;
 
 namespace EventSystem.VisualEditor.Nodes.Camera
@@ -10,35 +7,22 @@ namespace EventSystem.VisualEditor.Nodes.Camera
     /// <summary>
     /// DO NOT PUT ANY CODE HERE, WITH THE EXCEPTION OF EDITOR CODE
     /// </summary>
-    [NodeTint("#43AA8B")]
+    [NodeTint("#577590")]
     public class ChangeVirtualCameraNode : SkippableBaseNode
     {
         [Input] public NodeLink entry;
         [Output] public NodeLink exit;
 
-        [Tooltip("Documentation purposes only")] [TextArea]
+        [TextArea, Tooltip("Documentation purposes only")]
         public string description;
         
-        [OdinSerialize] public CinemachineVirtualCamera virtualCamera;
+        [Tooltip("Virtual camera to switch to")]
+        public CinemachineVirtualCamera virtualCamera;
 
-        [OdinSerialize] [Tooltip("Note: Custom does not work here, you would need to create it as a ScriptableObject")]
+        [Tooltip("Note: Custom does not work here, you would need to create it as a ScriptableObject")]
         public CinemachineBlendDefinition.Style blend;
 
-        [OdinSerialize] [Tooltip("Time to blend, default is 2")]
+        [Tooltip("Time to blend, default is 2")]
         public float blendTime = 2;
-
-#if UNITY_EDITOR
-        //TODO: Remove 'Button', create custom UI 
-        [Button("Create Virtual Cam")]
-        private void GenerateNewVirtualCamera()
-        {
-            var vcamGameObject = UnityEngine.Resources.Load<GameObject>("Prefabs/editorTools/CM vcam");
-            if (vcamGameObject == null) return;
-
-            //Assign object back to self
-            var instantiatedVcam = Utilities.InstantiateObject(vcamGameObject);
-            virtualCamera = instantiatedVcam.GetComponent<CinemachineVirtualCamera>();
-        }
-#endif
     }
 }
