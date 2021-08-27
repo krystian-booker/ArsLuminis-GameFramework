dd864d97bb21bc7032fe0f687e5eaf3198fbf96b
Krystian Booker <krystian.booker@gmail.com>
2021-08-26 20:00:00 -0400

    animation setup

--- Assets/Scripts/Characters/CharacterManager.cs
+++ Assets/Scripts/Characters/CharacterManager.cs
@@ -1,15 +1,23 @@
-﻿using EventSystem;
+﻿using System;
+using Characters.models;
+using EventSystem;
 using EventSystem.Triggers;
 using EventSystem.VisualEditor.Graphs;
 using Tools;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Assertions;
 
 namespace Characters
 {
     public class CharacterManager : MonoBehaviour
     {
         public CharacterStats characterStats;
-        
+
+        [Range(0.1f, 0.2f)] public float playerSpeed = 0.1f;
+
+        #region Event Sequence
+
         [Tooltip("Not required")] public EventSequenceSceneGraph defaultEventSequence;
         private EventTimelineParser _defaultEventTimelineParser;
 
@@ -19,9 +27,37 @@ namespace Characters
         [Range(1f, 20f)] public float focusRotationSpeed = 8f;
         private bool _focusActive;
         private GameObject _focusTarget;
-        
-        [Range(0.1f,0.2f)]
-        public float playerSpeed = 0.1f;
+
+        #endregion
+
+        #region Animations
+
+        public Animations animations;
+
+        [Tooltip("Disable if the 'character' does not have animations and as a result does not have an animator attached")]
+        public bool characterHasAnimations = true;
+
+        private float _activeSpeed;
+        private Vector3 _lastPosition;
+
+        #endregion
+
+        #region Components
+
+        private NavMeshAgent _navMeshAgent;
+        private Animator _animator;
+
+        #endregion
+
+        #region UnityEvents
+
+        private void Awake()
+        {
+            _navMeshAgent = GetComponent<NavMeshAgent>();
+            _animator = GetComponentInChildren<Animator>();
+            if (characterHasAnimations)
+                Assert.IsNotNull(_animator, $"{nameof(CharacterManager)}: Missing Animator on Character {gameObject.name}");
+        }
 
         private void Start()
         {
@@ -37,26 +73,10 @@ namespace Characters
         private void Update()
         {
             FocusOnTrigger();
-        }
-
-        /// <summary>
-        /// Used to pause an active eventSequence.
-        /// Only specific events can be paused that implement the IPauseEventExecution interface
-        /// </summary>
-        public void PauseEventSequence()
-        {
-            if (_defaultEventTimelineParser != null)
-            {
-                _defaultEventTimelineParser.PauseEventSequence();
-            }
-        }
-
-        /// <summary>
-        /// Used to resume an eventSequence that was paused.
-        /// </summary>
-        public void ResumeEventSequence()
-        {
-            _defaultEventTimelineParser.ResumeEventSequence();
+            UpdatePlayerSpeed();
+            
+            if (characterHasAnimations)
+                UpdateAnimations();
         }
 
         /// <summary>
@@ -95,6 +115,31 @@ namespace Characters
             StartCoroutine(npcEventTrigger.BeginTriggerEvent(this.gameObject, this));
         }
 
+        #endregion
+
+        /// <summary>
+        /// Used to pause an active eventSequence.
+        /// Only specific events can be paused that implement the IPauseEventExecution interface
+        /// </summary>
+        public void PauseEventSequence()
+        {
+            if (_defaultEventTimelineParser != null)
+            {
+                _defaultEventTimelineParser.PauseEventSequence();
+            }
+        }
+
+        /// <summary>
+        /// Used to resume an eventSequence that was paused.
+        /// </summary>
+        public void ResumeEventSequence()
+        {
+            if (_defaultEventTimelineParser != null)
+            {
+                _defaultEventTimelineParser.ResumeEventSequence();
+            }
+        }
+
         /// <summary>
         /// Sets the target this gameObject will rotate to look at until LoseFocus()
         /// is called
@@ -115,6 +160,13 @@ namespace Characters
             _focusTarget = null;
         }
 
+        private void UpdatePlayerSpeed()
+        {
+            var curMove = transform.position - _lastPosition;
+            _activeSpeed = curMove.magnitude / Time.deltaTime;
+            _lastPosition = transform.position;
+        }
+        
         /// <summary>
         /// Called from update, used to keep the 'Focus' on a target set with SetFocus()
         /// Until LoseFocus() is called, this gameObject will turn to look at the focus object
@@ -126,6 +178,17 @@ namespace Characters
             var lookRotation = Quaternion.LookRotation(direction);
             gameObject.transform.rotation =
                 Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * focusRotationSpeed);
+        }
+
+        private void UpdateAnimations()
+        {
+            var forward = transform.forward;
+            forward.y = 0;
+
+            var headingAngle = Quaternion.LookRotation(forward).eulerAngles.y;
+
+            _animator.SetFloat(animations.planarSpeedParameter, _activeSpeed);
+            _animator.SetFloat(animations.rotationParameter, headingAngle);
         }
     }
 }
---
+++ Assets/Scripts/Characters/models/Animations.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+namespace Characters.models
+{
+    [Serializable]
+    public class Animations
+    {
+        [Header("Locomotion")] 
+        [SerializeField] public string planarSpeedParameter = "PlanarSpeed";
+        [SerializeField] public string rotationParameter = "Rotation";
+    }
+}
