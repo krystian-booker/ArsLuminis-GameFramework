2246a4143b9c05b157e45063e4d8c5d4fc205a0d
Krystian Booker <krystian.booker@gmail.com>
2022-06-24 15:57:24 -0400

    Updated references

--- Assets/Scripts/EventSystem/Events/ChangeVirtualCameraExecution.cs
+++ Assets/Scripts/EventSystem/Events/ChangeVirtualCameraExecution.cs
@@ -32,33 +32,34 @@ namespace EventSystem.Events
             DisableVirtualCameras();
 
             //Set blend states
-            _cinemachineBrain.m_DefaultBlend.m_Style = cameraNode.blend;
-            _cinemachineBrain.m_DefaultBlend.m_Time = cameraNode.blendTime;
+            // _cinemachineBrain.m_DefaultBlend.m_Style = cameraNode.blend;
+            // _cinemachineBrain.m_DefaultBlend.m_Time = cameraNode.blendTime;
 
             //Set active camera state
             _timeStarted = Time.time;
-            cameraNode.virtualCamera.Priority = (int) CameraPriorityState.Active;
+            // cameraNode.virtualCamera.Priority = (int) CameraPriorityState.Active;
         }
 
         public bool IsFinished()
         {
-            return (Time.time - _timeStarted >= _cinemachineBrain.m_DefaultBlend.m_Time);
+            return true;
+            // return (Time.time - _timeStarted >= _cinemachineBrain.m_DefaultBlend.m_Time);
         }
 
         private void DisableVirtualCameras()
         {
-            for (var i = 0; i < CinemachineCore.Instance.VirtualCameraCount; i++)
-            {
-                var vcam = CinemachineCore.Instance.GetVirtualCamera(i);
-                if (vcam.Priority == (int) CameraPriorityState.Active)
-                {
-                    vcam.Priority = (int) CameraPriorityState.Secondary;
-                }
-                else
-                {
-                    vcam.Priority = (int) CameraPriorityState.Disabled;
-                }
-            }
+            // for (var i = 0; i < CinemachineCore.Instance.VirtualCameraCount; i++)
+            // {
+            //     var vcam = CinemachineCore.Instance.GetVirtualCamera(i);
+            //     if (vcam.Priority == (int) CameraPriorityState.Active)
+            //     {
+            //         vcam.Priority = (int) CameraPriorityState.Secondary;
+            //     }
+            //     else
+            //     {
+            //         vcam.Priority = (int) CameraPriorityState.Disabled;
+            //     }
+            // }
         }
     }
 }
