8b59d17b35622325e6ea04768b045bfea65f5f8e
Krystian Booker <krystian.booker@gmail.com>
2023-09-30 10:02:25 -0400

    Added json serialization to the SaveManager

--- Assets/Scripts/Managers/SaveManager.cs
+++ Assets/Scripts/Managers/SaveManager.cs
@@ -2,9 +2,9 @@
 using Assets.Scripts.Models.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Runtime.Serialization.Formatters.Binary;
 using System.Security.Cryptography;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -64,6 +64,13 @@ namespace Assets.Scripts.Managers
 
             string[] foundSaveFiles = Directory.GetFiles(saveFileDirectory, searchPattern);
 
+            if (foundSaveFiles.Length == 0)
+            {
+                saveCounter = 0;
+                return;
+            }
+
+            List<int> indices = new List<int>();
             foreach (string file in foundSaveFiles)
             {
                 string fileName = Path.GetFileName(file);
@@ -71,9 +78,70 @@ namespace Assets.Scripts.Managers
 
                 if (match.Success && int.TryParse(match.Groups[1].Value, out int index))
                 {
-                    saveCounter = Mathf.Max(saveCounter, index);
+                    indices.Add(index);
                 }
             }
+
+            // Set saveCounter to the maximum index found, or to 0 if no valid files were found.
+            saveCounter = indices.Any() ? indices.Max() : 0;
+        }
+
+        public List<SaveData> GetAllSaves()
+        {
+            string saveFileDirectory = Application.persistentDataPath;
+            string searchPattern = "saveData*.dat";
+
+            List<string> foundSaveFiles = Directory.GetFiles(saveFileDirectory, searchPattern).ToList();
+
+            List<SaveData> saveDataModels = new List<SaveData>();
+
+            if (foundSaveFiles.Contains(AutoSaveFilePath))
+            {
+                foundSaveFiles.Remove(AutoSaveFilePath);
+            }
+
+            // Loop through and create SaveDataModel objects for each save file.
+            foreach (var saveFile in foundSaveFiles)
+            {
+                var fileName = Path.GetFileName(saveFile);
+                var match = Regex.Match(fileName, @"saveData(\d+)_(\d+)\.dat");
+
+                if (match.Success)
+                {
+                    int.TryParse(match.Groups[1].Value, out int index);
+                    DateTime.TryParseExact(match.Groups[2].Value, "yyyyMMddHHmmss", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime dateTime);
+
+                    string screenshotPath = Path.ChangeExtension(saveFile, "png");
+
+                    saveDataModels.Add(new SaveData { Index = index, DateTime = dateTime, ScreenshotPath = screenshotPath, FileName = fileName });
+                }
+            }
+
+            // Sort the models list by DateTime in descending order.
+            saveDataModels = saveDataModels.OrderByDescending(model => model.DateTime).ToList();
+
+            // If autoSave exists, add it to the front of the list.
+            if (File.Exists(AutoSaveFilePath))
+            {
+                string autoSaveFileName = Path.GetFileName(AutoSaveFilePath);
+                string autoSaveDateTimeStr = autoSaveFileName.Split('_').LastOrDefault()?.Replace(".dat", string.Empty);
+
+                DateTime.TryParseExact(autoSaveDateTimeStr, "yyyyMMddHHmmss", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime autoSaveDateTime);
+
+                string autoSaveScreenshotPath = Path.ChangeExtension(AutoSaveFilePath, "png");
+
+                SaveData autoSaveDataModel = new SaveData
+                {
+                    Index = -1,
+                    DateTime = autoSaveDateTime,
+                    ScreenshotPath = autoSaveScreenshotPath,
+                    FileName = autoSaveFileName
+                };
+
+                saveDataModels.Insert(0, autoSaveDataModel);
+            }
+
+            return saveDataModels;
         }
 
         public void Save(bool isAutoSave = false, int overwriteIndex = -1)
@@ -83,6 +151,7 @@ namespace Assets.Scripts.Managers
                 try
                 {
                     string filePath;
+                    string dateTimeStr = DateTime.Now.ToString("yyyyMMddHHmmss");
 
                     if (isAutoSave)
                     {
@@ -90,51 +159,55 @@ namespace Assets.Scripts.Managers
                     }
                     else if (overwriteIndex >= 0)
                     {
-                        filePath = Path.Combine(Application.persistentDataPath, $"saveData{overwriteIndex}.dat"); // Overwrite existing save file.
+                        filePath = Path.Combine(Application.persistentDataPath, $"saveData{overwriteIndex}_{dateTimeStr}.dat"); // Overwrite existing save file.
                     }
                     else
                     {
-                        filePath = Path.Combine(Application.persistentDataPath, $"saveData{++saveCounter}.dat"); // Create new save file.
+                        saveCounter++;
+                        filePath = Path.Combine(Application.persistentDataPath, $"saveData{saveCounter}_{dateTimeStr}.dat"); // Create new save file.
                     }
 
-                    using (MemoryStream ms = new MemoryStream())
+                    // Capture Screenshot
+                    string screenshotPath = Path.ChangeExtension(filePath, "png");
+                    ScreenCapture.CaptureScreenshot(screenshotPath);
+
+                    var dataList = new List<Dictionary<string, object>>();
+                    foreach (var saveableObject in saveableObjects.Values)
                     {
-                        var bf = new BinaryFormatter();
-                        var dataList = new List<Dictionary<string, object>>();
+                        var data = saveableObject.Save();
+                        var serializableData = new Dictionary<string, object>();
 
-                        foreach (var saveableObject in saveableObjects.Values)
+                        foreach (var field in data.GetType().GetFields())
                         {
-                            var data = saveableObject.Save();
-                            var serializableData = new Dictionary<string, object>();
-
-                            foreach (var field in data.GetType().GetFields())
+                            if (field.FieldType == typeof(Vector3))
                             {
-                                if (field.FieldType == typeof(Vector3))
-                                {
-                                    Vector3 vector = (Vector3)field.GetValue(data);
-                                    serializableData.Add(field.Name, new SerializableVector3(vector));
-                                }
-                                else
-                                {
-                                    serializableData.Add(field.Name, field.GetValue(data));
-                                }
+                                Vector3 vector = (Vector3)field.GetValue(data);
+                                serializableData.Add(field.Name, new SerializableVector3(vector));
                             }
-
-                            dataList.Add(serializableData);
+                            else
+                            {
+                                serializableData.Add(field.Name, field.GetValue(data));
+                            }
                         }
 
-                        bf.Serialize(ms, dataList);
-                        var hexString = BitConverter.ToString(ms.ToArray()).Replace("-", string.Empty);
+                        dataList.Add(serializableData);
+                    }
 
-                        // Compute the checksum of the hex string.
-                        using (SHA256 sha256Hash = SHA256.Create())
-                        {
-                            byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(hexString));
-                            string checksum = BitConverter.ToString(bytes).Replace("-", string.Empty);
+                    // Serialize object to JSON string 
+                    string jsonString = JsonUtility.ToJson(dataList);
 
-                            // Save the checksum along with the hex string to the file.
-                            File.WriteAllText(filePath, $"{checksum}\n{hexString}");
-                        }
+                    // Convert JSON string to hex
+                    var jsonBytes = Encoding.UTF8.GetBytes(jsonString);
+                    var hexString = BitConverter.ToString(jsonBytes).Replace("-", string.Empty);
+
+                    // Compute the checksum of the hex string.
+                    using (SHA256 sha256Hash = SHA256.Create())
+                    {
+                        byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(hexString));
+                        string checksum = BitConverter.ToString(bytes).Replace("-", string.Empty);
+
+                        // Save the checksum along with the hex string to the file.
+                        File.WriteAllText(filePath, $"{checksum}\n{hexString}");
                     }
                 }
                 catch (Exception ex)
@@ -148,12 +221,12 @@ namespace Assets.Scripts.Managers
         {
             lock (LockObject)
             {
-                string loadPath = Path.Combine(Application.persistentDataPath, $"{fileName}.dat");
+                string loadPath = fileName.EndsWith(".dat") ?
+                                  Path.Combine(Application.persistentDataPath, fileName) :
+                                  Path.Combine(Application.persistentDataPath, $"{fileName}.dat");
 
                 if (File.Exists(loadPath))
                 {
-                    var bf = new BinaryFormatter();
-
                     try
                     {
                         // Read the checksum and hex string from the file.
@@ -180,43 +253,44 @@ namespace Assets.Scripts.Managers
                             }
                         }
 
-                        // Convert hex string to binary and deserialize.
-                        var binaryData = Enumerable.Range(0, hexString.Length)
-                            .Where(x => x % 2 == 0)
-                            .Select(x => Convert.ToByte(hexString.Substring(x, 2), 16))
-                            .ToArray();
+                        // Convert hex string to JSON bytes
+                        var jsonBytes = Enumerable.Range(0, hexString.Length)
+                                                  .Where(x => x % 2 == 0)
+                                                  .Select(x => Convert.ToByte(hexString.Substring(x, 2), 16))
+                                                  .ToArray();
 
-                        using (MemoryStream ms = new MemoryStream(binaryData))
+                        // Deserialize JSON string to object using Newtonsoft.Json
+                        var jsonString = Encoding.UTF8.GetString(jsonBytes);
+
+                        var dataList = JsonUtility.FromJson<List<Dictionary<string, object>>>(jsonString);
+
+                        foreach (var dataDict in dataList)
                         {
-                            var dataList = (List<Dictionary<string, object>>)bf.Deserialize(ms);
-
-                            foreach (var dataDict in dataList)
+                            if (dataDict.TryGetValue("Guid", out object guidObj) && guidObj is string guid && saveableObjects.TryGetValue(guid, out ISaveable saveableObject))
                             {
-                                if (dataDict.TryGetValue("Guid", out object guidObj) && guidObj is string guid && saveableObjects.TryGetValue(guid, out ISaveable saveableObject))
-                                {
-                                    var data = saveableObject.Save(); // Create an instance of appropriate SaveableData type.
+                                // Create an instance of appropriate SaveableData type.
+                                var data = saveableObject.Save();
 
-                                    foreach (var field in data.GetType().GetFields())
+                                foreach (var field in data.GetType().GetFields())
+                                {
+                                    if (dataDict.TryGetValue(field.Name, out object fieldValue))
                                     {
-                                        if (dataDict.TryGetValue(field.Name, out object fieldValue))
+                                        if (field.FieldType == typeof(Vector3) && fieldValue is SerializableVector3 serializableVector)
                                         {
-                                            if (field.FieldType == typeof(Vector3) && fieldValue is SerializableVector3 serializableVector)
-                                            {
-                                                field.SetValue(data, (Vector3)serializableVector);
-                                            }
-                                            else
-                                            {
-                                                field.SetValue(data, fieldValue);
-                                            }
+                                            field.SetValue(data, (Vector3)serializableVector);
+                                        }
+                                        else
+                                        {
+                                            field.SetValue(data, fieldValue);
                                         }
                                     }
+                                }
 
-                                    saveableObject.Load(data);
-                                }
-                                else
-                                {
-                                    Debug.LogError("Invalid GUID or no ISaveable object associated with it.");
-                                }
+                                saveableObject.Load(data);
+                            }
+                            else
+                            {
+                                Debug.LogError("Invalid GUID or no ISaveable object associated with it.");
                             }
                         }
                     }
---
+++ Assets/Scripts/Models/SaveData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Assets.Scripts.Models
+{
+    public class SaveData
+    {
+        public int Index { get; set; }
+        public DateTime DateTime { get; set; }
+        public string ScreenshotPath { get; set; }
+        public string FileName { get; set; }
+    }
+}
