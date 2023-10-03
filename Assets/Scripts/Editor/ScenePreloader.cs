449d347e618ca446f225c2bf92ec3c5424ef450b
Krystian Booker <krystian.booker@gmail.com>
2023-10-02 21:52:36 -0400

    Completed Scene Preloader

---
+++ Assets/Scripts/Editor/ScenePreloader.cs
@@ -0,0 +1,64 @@
+﻿using UnityEditor.SceneManagement;
+using UnityEditor;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts.Editor
+{
+    [InitializeOnLoad]
+    public static class ScenePreloader
+    {
+        private const string LastSceneKey = "LastScenePath";
+
+        static ScenePreloader()
+        {
+            EditorApplication.playModeStateChanged += PlayModeStateChanged;
+        }
+
+        public static string LastScenePath
+        {
+            get { return SessionState.GetString(LastSceneKey, string.Empty); }
+            set { SessionState.SetString(LastSceneKey, value); }
+        }
+
+        private static void PlayModeStateChanged(PlayModeStateChange state)
+        {
+            switch (state)
+            {
+                case PlayModeStateChange.ExitingEditMode:
+                    // Save the current scene
+                    if (EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
+                    {
+                        // Remember the current scene
+                        LastScenePath = EditorSceneManager.GetActiveScene().path;
+
+                        // Load the __preload scene
+                        EditorSceneManager.OpenScene("Assets/Scenes/__preload.unity");
+                    }
+                    else
+                    {
+                        // If the user cancels the save operation, don't proceed to Play mode
+                        EditorApplication.isPlaying = false;
+                    }
+                    break;
+
+                case PlayModeStateChange.EnteredPlayMode:
+                    if (!string.IsNullOrEmpty(LastScenePath))
+                    {
+                        // Load the previous scene after the __preload scene is loaded in play mode
+                        EditorSceneManager.LoadSceneAsyncInPlayMode(LastScenePath, new LoadSceneParameters(LoadSceneMode.Additive));
+                    }
+                    break;
+
+                case PlayModeStateChange.ExitingPlayMode:
+                    break;
+                case PlayModeStateChange.EnteredEditMode:
+                    // Load back the original scene after exiting play mode
+                    if (!string.IsNullOrEmpty(LastScenePath))
+                    {
+                        EditorSceneManager.OpenScene(LastScenePath);
+                    }
+                    break;
+            }
+        }
+    }
+}
